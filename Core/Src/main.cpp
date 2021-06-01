55cca955d5826378bab4c93dc12ec72c2897afac
Mikołaj <mikolaj.malecki@thaumatec.com>
2021-06-01 20:03:58 +0200

    add swv - not working

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -26,6 +26,8 @@
 #include "FreeRTOS.h"
 #include "FreeRTOSConfig.h"
 #include "task.h"
+
+#include "stdio.h"
 #include "MyClass.h"
 /* USER CODE END Includes */
 
@@ -57,7 +59,10 @@ static void db_led_task_handler(void* params);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+int __io_putchar(int ch){
+	ITM_SendChar(ch);
+	return ch;
+}
 /* USER CODE END 0 */
 
 /**
@@ -154,12 +159,34 @@ void SystemClock_Config(void)
 static void db_led_task_handler(void* params){
 
 	while(1){
+		printf("hello\n");
 		HAL_GPIO_TogglePin(DB_LED_GPIO_Port, DB_LED_Pin);
-		vTaskDelay(100);
+		vTaskDelay(1000);
 	}
 }
 
 /* USER CODE END 4 */
+
+/**
+ * @brief  Period elapsed callback in non blocking mode
+ * @note   This function is called  when TIM5 interrupt took place, inside
+ * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+ * a global variable "uwTick" used as application time base.
+ * @param  htim : TIM handle
+ * @retval None
+ */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+ /* USER CODE BEGIN Callback 0 */
+
+ /* USER CODE END Callback 0 */
+ if (htim->Instance == TIM5) {
+   HAL_IncTick();
+ }
+ /* USER CODE BEGIN Callback 1 */
+
+ /* USER CODE END Callback 1 */
+}
 
 /**
   * @brief  This function is executed in case of error occurrence.
