0756b67a3edca957582b830eec83018ae07bc287
Mikołaj <mikolaj.malecki@thaumatec.com>
2021-07-17 12:27:17 +0200

    add epty heart rate class

---
+++ Core/Src/MAX30102/HearRate.cpp
@@ -0,0 +1,18 @@
+/*
+ * HearRate.cpp
+ *
+ *  Created on: Jul 12, 2021
+ *      Author: maskopol
+ */
+
+#include <HearRate.h>
+
+HearRate::HearRate() {
+	// TODO Auto-generated constructor stub
+
+}
+
+HearRate::~HearRate() {
+	// TODO Auto-generated destructor stub
+}
+
