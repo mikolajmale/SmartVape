6ee08cdfca8100d1b2ff1c71c692617c99efbfc6
Mikołaj <mikolaj.malecki@thaumatec.com>
2021-07-24 01:35:51 +0200

    refactor

--- scripts/clean_serial_refactored.py
+++ scripts/clean_serial_refactored.py
@@ -5,11 +5,6 @@ from typing import List
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-class ParseTypes(Enum):
-    INT = 1
-    FLOAT = 2
-    CHAR = 3
-
 
 class SerialLineParser:
     def __init__(self, delimeter=',', arg_num: int = 2):
@@ -35,41 +30,46 @@ class SerialLineParser:
     def valid(self):
         return self.__last_valid
 
-# plot class
+    @property
+    def param_num(self):
+        return self.__arg_num
+
+
 class AnalogPlot:
-    # constr
-    def __init__(self, parser=SerialLineParser(), strPort='/dev/ttyACM0', baud=115200, timeout=10, maxLen=500):
+    def __init__(self, ax, parser=SerialLineParser(), strPort='/dev/ttyACM0', baud=115200, timeout=10, maxLen=500):
         # open serial port
         self.ser = serial.Serial(strPort, baud, timeout=timeout)
 
         self.parser = parser
-        self.first = deque([0] * maxLen)
-        self.second = deque([0] * maxLen)
+        self.signals = [deque([0] * maxLen) for i in range(parser.param_num)]
+        self.ax = ax
         self.maxLen = maxLen
+        self.axes = []
+        for i in range(parser.param_num):
+            a, = ax.plot([], [])
+            self.axes.append(a)
 
-    # add to buffer
-    def addToBuf(self, buf, val):
+    def __add_to_buf(self, buf, val):
         if len(buf) < self.maxLen:
             buf.append(val)
         else:
             buf.pop()
             buf.appendleft(val)
 
-    # add data
     def add(self, data):
-        self.addToBuf(self.first, data[0])
-        self.addToBuf(self.second, data[1])
+        for i, s in enumerate(self.signals):
+            self.__add_to_buf(s, data[i])
 
     # update plot
-    def update(self, frameNum, a0, a1):
+    def update(self, frameNum):
         try:
             line = self.ser.readline().decode('utf-8').rstrip()
             data = self.parser(line)
             self.add(data)
             print(f'{data}')
 
-            a0.set_data(range(self.maxLen), self.first)
-            a1.set_data(range(self.maxLen), self.second)
+            self.axes[0].set_data(range(self.maxLen), self.signals[0])
+            self.axes[1].set_data(range(self.maxLen), self.signals[1])
         except KeyboardInterrupt:
             print('exiting')
 
@@ -78,25 +78,16 @@ class AnalogPlot:
         self.ser.flush()
         self.ser.close()
 
-    # main() function
-
 
 def main():
-    analogPlot = AnalogPlot()
-
-    print('plotting data...')
-
     # set up animation
     fig = plt.figure()
     ax = plt.axes()
-    ax.set_xlim(0, 1000)
+    ax.set_xlim(0, 500)
     ax.set_ylim(0, 1500)
+    analogPlot = AnalogPlot(ax)
 
-    a0, = ax.plot([], [])
-    a1, = ax.plot([], [])
-    anim = animation.FuncAnimation(fig, analogPlot.update,
-                                   fargs=(a0, a1),
-                                   interval=5)
+    anim = animation.FuncAnimation(fig, analogPlot.update, interval=5)
 
     # show plot
     plt.show()
